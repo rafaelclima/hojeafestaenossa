b2c63943866d52498da1405770ebaaa94c7b7436
Rafael Lima <rafaelclima.ti@gmail.com>
2026-02-26 16:58:02 -0300

    Adiciona estrutura inicial do projeto com Maven, incluindo arquivos de configuração, classes principais e dependências do Spring Boot

---
+++ src/main/java/com/rafaellima/hojeafestaenossa/HojeafestaenossaApplication.java
@@ -0,0 +1,13 @@
+package com.rafaellima.hojeafestaenossa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HojeafestaenossaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HojeafestaenossaApplication.class, args);
+	}
+
+}
