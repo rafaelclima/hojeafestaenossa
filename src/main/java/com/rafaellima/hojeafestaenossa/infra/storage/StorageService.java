f231df6b1afd3b3c47fd079baee2debbd0ac89e3
Rafael Lima <rafaelclima.ti@gmail.com>
2026-03-01 16:50:28 -0300

    feat: cria a interface StorageService para gerenciar uploads de arquivos

---
+++ src/main/java/com/rafaellima/hojeafestaenossa/infra/storage/StorageService.java
@@ -0,0 +1,13 @@
+package com.rafaellima.hojeafestaenossa.infra.storage;
+
+import java.io.InputStream;
+
+public interface StorageService {
+
+    String upload(
+            String objectName,
+            InputStream inputStream,
+            long contentLength,
+            String contentType);
+
+}
