9a322efba520b100a61af1475e5a4a2b3952f5c6
Rafael Lima <rafaelclima.ti@gmail.com>
2026-03-01 16:28:12 -0300

    feat: cria o repositório EventRepository para gerenciar eventos no banco de dados

---
+++ src/main/java/com/rafaellima/hojeafestaenossa/event/repository/EventRepository.java
@@ -0,0 +1,14 @@
+package com.rafaellima.hojeafestaenossa.event.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rafaellima.hojeafestaenossa.event.domain.Event;
+
+public interface EventRepository extends JpaRepository<Event, UUID> {
+
+    Optional<Event> findByAccessToken(String accessToken);
+
+}
