75d8a3d1d9bc68cfdb81f0d2a33bf587b180f23a
Rafael Lima <rafaelclima.ti@gmail.com>
2026-03-01 16:28:37 -0300

    feat: adiciona configuração JPA com auditoria habilitada

---
+++ src/main/java/com/rafaellima/hojeafestaenossa/shared/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.rafaellima.hojeafestaenossa.shared.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
