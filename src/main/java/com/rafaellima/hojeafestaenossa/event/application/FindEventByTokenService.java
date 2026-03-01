d7deac25c9ef580d97f20d00fb05c483db0a65be
Rafael Lima <rafaelclima.ti@gmail.com>
2026-03-01 16:27:42 -0300

    feat: cria o serviço FindEventByToken para buscar eventos pelo token de acesso

---
+++ src/main/java/com/rafaellima/hojeafestaenossa/event/application/FindEventByTokenService.java
@@ -0,0 +1,21 @@
+package com.rafaellima.hojeafestaenossa.event.application;
+
+import org.springframework.stereotype.Service;
+
+import com.rafaellima.hojeafestaenossa.event.domain.Event;
+import com.rafaellima.hojeafestaenossa.event.repository.EventRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class FindEventByTokenService {
+
+    private final EventRepository eventRepository;
+
+    public Event execute(String accessToken) {
+        return eventRepository.findByAccessToken(accessToken)
+                .orElseThrow(() -> new RuntimeException("Event not found"));
+    }
+
+}
