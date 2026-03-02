8907873c5acc9478ad86ecdbe0e7374145bde597
Rafael Lima <rafaelclima.ti@gmail.com>
2026-03-02 12:17:34 -0300

    feat: criar classe BusinessException para tratamento de exceções personalizadas

---
+++ src/main/java/com/rafaellima/hojeafestaenossa/shared/exception/BusinessException.java
@@ -0,0 +1,9 @@
+package com.rafaellima.hojeafestaenossa.shared.exception;
+
+public class BusinessException extends ExceptionCustomized {
+
+    public BusinessException(String code, String message) {
+        super(code, message);
+    }
+
+}
