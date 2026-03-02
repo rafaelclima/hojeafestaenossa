42aa4502c12da655f8415637b26ddfc5c8371de3
Rafael Lima <rafaelclima.ti@gmail.com>
2026-03-02 12:27:25 -0300

    feat: exception notfound personalizada criado

---
+++ src/main/java/com/rafaellima/hojeafestaenossa/shared/exception/NotFoundException.java
@@ -0,0 +1,9 @@
+package com.rafaellima.hojeafestaenossa.shared.exception;
+
+public class NotFoundException extends ExceptionCustomized {
+
+    public NotFoundException(String code, String message) {
+        super(code, message);
+    }
+
+}
