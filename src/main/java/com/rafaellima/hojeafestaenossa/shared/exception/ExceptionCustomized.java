f0172f4d3b94c648eadcb02ee1972e5277e34bba
Rafael Lima <rafaelclima.ti@gmail.com>
2026-03-02 12:17:48 -0300

    feat: criar classe ExceptionCustomized para tratamento de exceções personalizadas

---
+++ src/main/java/com/rafaellima/hojeafestaenossa/shared/exception/ExceptionCustomized.java
@@ -0,0 +1,19 @@
+package com.rafaellima.hojeafestaenossa.shared.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ExceptionCustomized extends RuntimeException {
+
+    private String code;
+    private String message;
+
+    public ExceptionCustomized(String code, String message) {
+        super();
+        this.code = code;
+        this.message = message;
+    }
+
+}
