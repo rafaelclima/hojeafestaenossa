0be231f8248060e2e3f6a400dee8d7e5b9ee988b
Rafael Lima <rafaelclima.ti@gmail.com>
2026-03-02 12:27:44 -0300

    feat: exception technical personalizada criado

---
+++ src/main/java/com/rafaellima/hojeafestaenossa/shared/exception/TechnicalException.java
@@ -0,0 +1,9 @@
+package com.rafaellima.hojeafestaenossa.shared.exception;
+
+public class TechnicalException extends ExceptionCustomized {
+
+    public TechnicalException(String code, String message) {
+        super(code, message);
+    }
+
+}
